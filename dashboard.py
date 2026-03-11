9d203a0923f804ce5dfa3a97787186a26f0fc2b3
Lucho De Gregorio <luis.degregorio55@gmail.com>
2026-03-10 23:51:05 -0300

    uso de secrets para supabase

--- dashboard.py
+++ dashboard.py
@@ -13,8 +13,8 @@ st.title("Sistema de Monitoreo Inteligente")
 st_autorefresh(interval=10000, key="refresh")
 
 # conexión
-url = "https://anwwqipwfcfycravtxvb.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFud3dxaXB3ZmNmeWNyYXZ0eHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNjI5NjMsImV4cCI6MjA4ODYzODk2M30.BPAYx_hbNI2sSQhUC_BhenQ5Yce0fAsXc33Cpmyn_JM"
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
 
