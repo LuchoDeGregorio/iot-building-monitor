03e9b868adfbf35c2f285e2baf71de64cfef8a6c
Lucho De Gregorio <luis.degregorio55@gmail.com>
2026-03-11 19:55:11 -0300

    correccion refresh dashboard

--- dashboard.py
+++ dashboard.py
@@ -12,9 +12,13 @@ st.title("Sistema de Monitoreo Inteligente")
 # refresco automático
 st_autorefresh(interval=10000, key="refresh")
 
-# conexión
+
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_KEY"]
+
+# conexión
+#url = "SUPABASE_URL","https://anwwqipwfcfycravtxvb.supabase.co"
+#key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFud3dxaXB3ZmNmeWNyYXZ0eHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNjI5NjMsImV4cCI6MjA4ODYzODk2M30.BPAYx_hbNI2sSQhUC_BhenQ5Yce0fAsXc33Cpmyn_JM"
 
 supabase = create_client(url, key)
 
